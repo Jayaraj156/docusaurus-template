a235a24f20d236b7a7cc8fa5076f041706da5211
AURIGO-TnD <86467657+AURIGO-TnD@users.noreply.github.com>
2021-06-25 16:47:54 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
     footer: {
       style: 'dark',
       links: [],
-      copyright: `Copyright © ${new Date().getFullYear()}.`,
+      copyright: `© Aurigo Software Technologies Inc. ${new Date().getFullYear()}.`,
     },
   },
   presets: [
